c0a0634c2a3213d43017d911fa76675cfd5858ef
RenatoNandes <renato.nandes@estudante.ufjf.br>
2025-11-18 16:04:40 -0300

    Cria a class Jogada e os seus atributos

--- src/main/java/br/ufjf/dcc/ciberduelo/modelo/Jogada.java
+++ src/main/java/br/ufjf/dcc/ciberduelo/modelo/Jogada.java
@@ -1,4 +1,22 @@
 package br.ufjf.dcc.ciberduelo.modelo;
 
+import java.util.List;
+
 public class Jogada {
+
+    private final int turno;
+    private final String jogadorId;
+    private final String adversarioId;
+    private final List<Carta> cartasJogadas;
+    private final boolean passou;
+    private final boolean desistiu;
+    private final int vidaAntesJogador;
+    private final int vidaDepoisJogador;
+    private final int energiaAntesJogador;
+    private final int energiaDepoisJogador;
+    private final int vidaAntesAdversario;
+    private final int vidaDepoisAdversario;
+    private final int energiaAntesAdversario;
+    private final int energiaDepoisAdversario;
+
 }
