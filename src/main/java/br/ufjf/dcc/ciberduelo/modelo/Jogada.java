bf546cfa866fd824edeafd6195d2026101939950
RenatoNandes <renato.nandes@estudante.ufjf.br>
2025-11-18 16:19:11 -0300

    Cria a classe estatica Builder e os seus atributos e métodos

--- src/main/java/br/ufjf/dcc/ciberduelo/modelo/Jogada.java
+++ src/main/java/br/ufjf/dcc/ciberduelo/modelo/Jogada.java
@@ -37,4 +37,101 @@ public class Jogada {
         this.energiaDepoisAdversario = builder.energiaDepoisAdversario;
     }
 
+    public static class Builder { // Classe Builder para montar a jogada passo a passo
+
+        private int turno;
+        private String jogadorId;
+        private String adversarioId;
+        private List<Carta> cartasJogadas;
+        private boolean passou;
+        private boolean desistiu;
+        private int vidaAntesJogador;
+        private int vidaDepoisJogador;
+        private int energiaAntesJogador;
+        private int energiaDepoisJogador;
+        private int vidaAntesAdversario;
+        private int vidaDepoisAdversario;
+        private int energiaAntesAdversario;
+        private int energiaDepoisAdversario;
+
+        // Métodos da classe Builder
+
+        public Builder() {
+        }
+
+        public Builder turno(int turno) {
+            this.turno = turno;
+            return this;
+        }
+
+        public Builder jogadorId(String jogadorId) {
+            this.jogadorId = jogadorId;
+            return this;
+        }
+
+        public Builder adversarioId(String adversarioId) {
+            this.adversarioId = adversarioId;
+            return this;
+        }
+
+        public Builder cartasJogadas(List<Carta> cartasJogadas) {
+            this.cartasJogadas = cartasJogadas;
+            return this;
+        }
+
+        public Builder passou(boolean passou) {
+            this.passou = passou;
+            return this;
+        }
+
+        public Builder desistiu(boolean desistiu) {
+            this.desistiu = desistiu;
+            return this;
+        }
+
+        public Builder vidaAntesJogador(int vida) {
+            this.vidaAntesJogador = vida;
+            return this;
+        }
+
+        public Builder vidaDepoisJogador(int vida) {
+            this.vidaDepoisJogador = vida;
+            return this;
+        }
+
+        public Builder energiaAntesJogador(int energia) {
+            this.energiaAntesJogador = energia;
+            return this;
+        }
+
+        public Builder energiaDepoisJogador(int energia) {
+            this.energiaDepoisJogador = energia;
+            return this;
+        }
+
+        public Builder vidaAntesAdversario(int vida) {
+            this.vidaAntesAdversario = vida;
+            return this;
+        }
+
+        public Builder vidaDepoisAdversario(int vida) {
+            this.vidaDepoisAdversario = vida;
+            return this;
+        }
+
+        public Builder energiaAntesAdversario(int energia) {
+            this.energiaAntesAdversario = energia;
+            return this;
+        }
+
+        public Builder energiaDepoisAdversario(int energia) {
+            this.energiaDepoisAdversario = energia;
+            return this;
+        }
+
+        public Jogada build() {
+            return new Jogada(this);
+        }
+    }
+
 }
